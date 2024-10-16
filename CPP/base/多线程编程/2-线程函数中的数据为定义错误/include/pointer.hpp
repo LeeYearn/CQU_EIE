01adee07e5513b8ebc625c31e5b47f26d394f4ff
LiiiiiiY <1317262104@qq.com>
2024-10-16 10:16:39 +0800

    reconstruction

--- CPP/base/多线程编程/2-线程函数中的数据为定义错误/include/pointer.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <thread>
-#include <string>
-
-void foo(int *p);
-void pointer();
--- CPP/base/多线程编程/2-线程函数中的数据为定义错误/include/quote.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <thread>
-#include <string>
-
-void foo1(int &a);
-void quote();
