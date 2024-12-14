5b2301f55bf5d1f23266df2c690c0853a2257e5f
LeeYearn <isleeyearn@outlook.com>
2024-12-14 12:36:16 +0800

    update

--- CPP/sumofTwoNumbers/main.cpp
+++ CPP/sumofTwoNumbers/main.cpp
@@ -3,23 +3,38 @@
 #include <stack>
 #include <cmath>
 #include <vector>
+#include <unordered_map>
 
-std::vector<int> twoSum(std::vector<int> &nums, int target)
+std::vector<int> twoSumEnum(std::vector<int> &nums, int target)
 {
     int length = nums.size();
-    std::vector<int> result;
     for (int i = 0; i < length; i++)
         for (int j = i + 1; j < length; j++)
             if (nums[i] + nums[j] == target)
-                return result = {i, j};
-    return result;
+                return {i, j};
+    return {};
+}
+
+std::vector<int> twoSumHash(std::vector<int> &nums, int target)
+{
+    int length = nums.size();
+    std::unordered_map<int, int> hashtable;
+    for (int i = 0; i < length; ++i)
+    {
+        auto it = hashtable.find(target - nums[i]);
+        if (it != hashtable.end())
+            return {it->second, i};
+        hashtable[nums[i]] = i;
+    }
+    return {};
 }
 
 int main()
 {
-    std::vector<int> nums = {-3,4,3,90};
+    std::vector<int> nums = {-3, 4, 3, 90};
     int target = 0;
-    std::vector<int> answer = twoSum(nums, target);
+    // std::vector<int> answer = twoSumEnum(nums, target);
+    std::vector<int> answer = twoSumHash(nums, target);
     std::cout << "answer: " << answer[0] << ", " << answer[1] << std::endl;
     return 0;
 }
